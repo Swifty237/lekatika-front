76b1063c7e38ab3842dd5a99c607079460ab34d7
Yannick <yannickkamdemkouam@yahoo.fr>
2026-05-19 00:50:22 +0200

    Project initialization

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+// src/App.tsx
+function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-3xl font-bold text-blue-600">
+        Bienvenue sur mon projet React + TypeScript + Tailwind
+      </h1>
+    </div>
+  );
+}
+
+export default App;
